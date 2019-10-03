f529b6c8c35cc981cfef93d875023aa9481381b8
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2019-10-03 19:23:43 -0400

    Fixed search dialog

--- source/class/omna/form/dialog/Search.js
+++ source/class/omna/form/dialog/Search.js
@@ -42,25 +42,23 @@ qx.Class.define("omna.form.dialog.Search", {
                         widget.setWidth && widget.setWidth(Math.floor(this.getWidth() * 67 / 100));
                         widget.setFromJSON(field);
 
-                        if (qx.Class.hasProperty(widget.constructor, 'required')) {
-                            widget.setRequired(false);
-                        }
+                        if (qx.Class.hasProperty(widget.constructor, 'required')) widget.setRequired(false);
 
                         if (field.validatorClass) {
                             var validator,
                                 validatorClass = qx.Class.getByName(field.validatorClass);
 
-                            if (!validatorClass) {
-                                q.messaging.emit("Application", "error", this.tr("Class no found: '%1'.", field.validatorClass));
-                            } else {
+                            if (validatorClass) {
                                 validator = new validatorClass();
+                            } else {
+                                q.messaging.emit("Application", "error", this.tr("Class no found: '%1'.", field.validatorClass));
                             }
                         } else {
                             validator = widgetClass.validatorClass ? new widgetClass.validatorClass : null;
                         }
 
                         var label = this.i18nTrans(field.label || field.name);
-                        form.add(widget, label, validator, field.searchAttrName || field.name, form);
+                        form.add(widget, label, validator, field.attrModelName || field.name, form);
 
                     } else {
                         q.messaging.emit("Application", "error", this.tr("Class no found: '%1'.", field.widgetClass));
