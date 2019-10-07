da00ada99a1c7a3edc8ade41bd9e869aeec03cea
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2019-10-07 10:43:21 -0400

    Fixed print order documents

--- source/class/omna/action/order/Print.js
+++ source/class/omna/action/order/Print.js
@@ -2,7 +2,7 @@
  * @asset(qx/icon/${qx.icontheme}/16/actions/document-print.png)
  */
 qx.Class.define('omna.action.order.Print', {
-    extend: omna.action.AbstractActionWithSelectedItem,
+    extend: omna.action.AbstractAction,
 
     construct: function (management) {
         this.base(arguments, management, 'print', 'icon/16/actions/document-print.png');
@@ -10,18 +10,7 @@ qx.Class.define('omna.action.order.Print', {
 
     members: {
         onExecute: function () {
-            var order = this.getSelectedItem(),
-                module = { id: 'OrderPrintDocuments', i18n: 'Orders', 'verticalLayout': false },
-                data = {
-                    order: order,
-                    label: this.i18nTrans('MODULE-REFERENCE-PRINT-DOCUMENTS', [order.number])
-                };
-
-            q.messaging.emit('Application', 'open-module', module, data);
-        },
-
-        onAccept: function (e) {
-            this.emitMessaging('execute-update', e.getData(), { dlg: e.getTarget() });
+            this.emitMessaging('execute-print');
         }
     }
 });
--- source/class/omna/management/order/Documents.js
+++ source/class/omna/management/order/Documents.js
@@ -26,13 +26,14 @@ qx.Class.define("omna.management.order.Documents", {
     members: {
         getActions: function () {
             var a1 = new qx.ui.basic.Label(this.i18nTrans('select_document_type')),
-                a2 = this.__documentSelectBox = new omna.action.order.DocumentSelectBox();
+                a2 = this.__documentSelectBox = new omna.action.order.DocumentSelectBox(),
+                a3 = this.__documentPrint = new omna.action.order.Print(this);
 
             a1.set({ padding: [2, 5], decorator: 'main' });
 
             this.__documentSelectBox.addListener("changeSelection", this.onDocumentChangeSelection, this);
 
-            return [a1, a2];
+            return [a1, a2, a3];
         },
 
         __createContent: function (customData) {
@@ -40,13 +41,16 @@ qx.Class.define("omna.management.order.Documents", {
             this.__docuemnetFrame = new qx.ui.embed.Iframe();
 
             this.__docuemnetTip.set({ textColor: 'black', autoHide: false, maxHeight: 70 });
+            this.__docuemnetFrame.set({ padding: 10 });
 
             this.add(this.__docuemnetTip, { flex: 2 });
-            this.add(this.__docuemnetFrame, { flex: 3 });
+            this.add(this.__docuemnetFrame, { flex: 2 });
         },
 
-        __createObjectURL: function (data, contentType) {
-            var blod = new Blob([data], { type: contentType }),
+        __createObjectURL: function (data) {
+            var file = qx.util.Base64.decode(data.file),
+                contentType = data.mime_type,
+                blod = new Blob([file], { type: contentType }),
                 objUrl = window.URL.createObjectURL(blod);
 
             return objUrl;
@@ -70,10 +74,8 @@ qx.Class.define("omna.management.order.Documents", {
 
                 request.getOrderDoc(order, docType, function (response) {
                     if (response.successful) {
-                        this.__docuemnetTip.hide();
-                        this.__docuemnetFrame.set({
-                            source: this.__createObjectURL(response.data, response.headers['content-type'])
-                        });
+                        this.__docuemnetTip.exclude();
+                        this.__docuemnetFrame.set({ source: this.__createObjectURL(response.data) });
                     } else {
                         this.__docuemnetTip.set({
                             label: response.message, icon: 'omna/icon/32/warn.png', backgroundColor: 'warn'
@@ -82,6 +84,12 @@ qx.Class.define("omna.management.order.Documents", {
                     }
                 }, this);
             }
+        },
+
+        onExecutePrint: function () {
+            var iframe = this.__docuemnetFrame.getContentElement().getDomElement();
+            iframe.focus();
+            iframe.contentWindow.print();
         }
     }
 });
