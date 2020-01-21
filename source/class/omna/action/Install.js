3df64ba5ff089c9d0bcb94880a9e012ff3bf917f
Yoandry Pacheco Aguila <yoandrypa@gmail.com>
2020-01-21 12:06:59 -0500

    Add
    Module to management collections

---
+++ source/class/omna/action/Install.js
@@ -0,0 +1,25 @@
+/**
+ * @asset(omna/icon/16/actions/go-bottom.png)
+ */
+qx.Class.define("omna.action.Install", {
+    extend: omna.action.AbstractActionWithSelectedItem,
+
+    construct: function (management) {
+        this.base(arguments, management, 'install', 'omna/icon/16/actions/go-bottom.png');
+    },
+
+    members: {
+        onExecute: function () {
+            this.setEnabled(false);
+
+            var management = this.getManagement(),
+                itemLabel = this.i18nTrans('SINGLE-ITEM-REFERENCE'),
+                msg = this.i18nTrans('Messages', 'CONFIRM-INSTALL', [itemLabel]);
+
+            omna.dialog.Confirm.show(msg, function (response) {
+                if (response === 'yes') management.getRequestManagement().install(this.getSelectedItem().id);
+                this.setEnabled(true);
+            }, this);
+        }
+    }
+});
--- source/class/omna/request/Collections.js
+++ source/class/omna/request/Collections.js
@@ -6,8 +6,25 @@ qx.Class.define("omna.request.Collections", {
     },
 
     members: {
-        install: function (id) {
-            alert('TODO:..')
+        getI18nCatalog: function () {
+          return 'Collections'
+        },
+
+        install: function (id, callBack, scope) {
+            // Call remote service
+            this.submit("PATCH", id, null, function (response) {
+                var msg;
+
+                if (response.successful) {
+                    msg = this.i18nTrans('Messages', 'SUCCESSFUL-INSTALLING');
+                    q.messaging.emit('Application', 'good', msg);
+                } else {
+                    msg = this.i18nTrans('Messages', 'FAILED-INSTALLING');
+                    q.messaging.emit('Application', 'error', msg);
+                }
+
+                callBack && callBack.call(scope, response);
+            }, this);
         },
 
         uninstall: function (id, callBack, scope) {
