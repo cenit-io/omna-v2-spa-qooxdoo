93bd8c3373f5d32471500b7d6d7cfc9a34f16f3d
yoandrypa <yoandrypa@gmail.com>
2021-09-23 10:28:29 -0400

    Fixed MultiSelectBox

--- source/class/omna/form/field/EnumSelectBox.js
+++ source/class/omna/form/field/EnumSelectBox.js
@@ -1,5 +1,5 @@
 qx.Class.define("omna.form.field.EnumSelectBox", {
-  extend: omna.form.field.util.AbstractSelectBox,
+  extend: omna.form.field.MultiSelectBox,
 
   statics: {
     cellRendererClass: omna.table.cellrenderer.String
@@ -20,37 +20,6 @@ qx.Class.define("omna.form.field.EnumSelectBox", {
   },
 
   members: {
-    _applyOptions: function (items) {
-      items.forEach(function (item) {
-        let label, model, icon;
 
-        if (qx.lang.Type.isObject(item)) {
-          model = item.value;
-          label = item.label || model;
-          icon = item.icon;
-
-          if (item.i18n) label = omna.I18n.trans(item.i18n, 'Labels', label)
-        } else {
-          label = model = item;
-        }
-
-        icon = icon || null;
-
-        this.add(new omna.form.field.ListItem(label, icon, model));
-      }, this);
-    },
-
-    // overridden
-    _createChildControlImpl: function (id, hash) {
-      let control;
-
-      switch ( id ) {
-        case "list":
-          control = this.base(arguments, id).set({ selectionMode: "additive" });
-          break;
-      }
-
-      return control || this.base(arguments, id);
-    },
   }
 });
